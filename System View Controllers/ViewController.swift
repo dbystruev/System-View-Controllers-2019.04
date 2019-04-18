323c12161c31f9de5ddfc2d08879a36c3178e1c1
Denis Bystruev <dbystruev@me.com>
2019-04-18 20:17:53 +0300

    Initial Commit

---
+++ System View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System View Controllers
+//
+//  Created by Denis Bystruev on 18/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
